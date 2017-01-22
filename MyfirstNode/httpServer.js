00fc65911b06c57ada8347e1bf55261d6fa88f73
Галя Георгиева <alice.gospodinova@gmail.com>
2017-01-22 14:43:01 +0200

    Add favicon.ico

--- MyfirstNode/httpServer.js
+++ MyfirstNode/httpServer.js
@@ -1,21 +1,41 @@
 let http = require('http')
 let fs = require('fs')
+let port = 1337
+
 http
 .createServer((request, response) => {
+  console.log(request.url)
+  // console.log(request.headers)
+
   if (request.method === 'GET') {
-    fs.readFile('./index.html', (err, data) => {
-      if (err) { console.log(err) } else {
-        response.writeHead(200, {
-          'content-type': 'text/html'
-        })
-        response.write(data)
-        response.end()
-      }
-    })
+    if (request.url === '/favicon.ico') {
+      fs.readFile('./favicon.ico', (err, data) => {
+        if (err) { console.log(err) } else {
+          response.writeHead(200)
+          response.write(data)
+          response.end()
+        }
+      })
+    } else if (request.url === '/') {
+      fs.readFile('./index.html', (err, data) => {
+        if (err) { console.log(err) } else {
+          response.writeHead(200, {
+            'content-type': 'text/html'
+          })
+          response.write(data)
+          response.end()
+        }
+      })
+    } else {
+      response.writeHead(404)
+      response.write('404 URL not found')
+    }
   } else if (request.method === 'POST') {
     response.writeHead(500)
     response.write('POST method not supported')
     response.end()
   }
 })
-.listen(1337)
+.listen(port)
+
+console.log(`Server listening on port ${port}`)
