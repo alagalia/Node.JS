26599447adba213c6a38dca293fc1caf350f7d5b
Галя Георгиева <alice.gospodinova@gmail.com>
2017-01-22 12:13:13 +0200

    Wrote logic for request

--- MyfirstNode/httpServer.js
+++ MyfirstNode/httpServer.js
@@ -2,10 +2,16 @@ let http = require('http')
 
 http
 .createServer((request, response) => {
-  response.writeHead(200, {
-    'content-type': 'text/html'
-  })
-  response.write('<h1>Hi from node!</h1>')
-  response.end()
+  if (request.method === 'POST') {
+    response.writeHead(200, {
+      'content-type': 'text/html'
+    })
+    response.write('<h1>Hi from POST method!</h1>')
+    response.end()
+  } else if (request.method === 'GET') {
+    response.writeHead(500)
+    response.write('GET method not supported')
+    response.end()
+  }
 })
 .listen(1337)
