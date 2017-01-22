cfc17266cc5b07b90189cbee30390373cba5813f
Галя Георгиева <alice.gospodinova@gmail.com>
2017-01-22 12:03:32 +0200

    Add first header for response

--- MyfirstNode/httpServer.js
+++ MyfirstNode/httpServer.js
@@ -2,7 +2,9 @@ let http = require('http')
 
 http
 .createServer((request, response) => {
-  response.writeHead(200)
+  response.writeHead(200, {
+    'content-type': 'text/html'
+  })
   response.write('<h1>Hi from node!</h1>')
   response.end()
 })
